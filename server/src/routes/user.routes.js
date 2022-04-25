cac0add60b32a5d549815c6b5e0b735444493bcc
juancamilosalazarrestrepo <juancamilosalazarrestrepo@gmail.com>
2022-04-25 18:32:00 -0500

    agregados post put y delete

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -10,4 +10,22 @@ router.get('/', (req,res)=>{
 
 });
 
+
+router.post('/', (req,res) => {
+    res.json('Post to user endpoint')
+
+} );
+
+router.put('/', (req,res) => {
+    res.json('Put to user endpoint')
+
+} );
+
+router.delete('/', (req,res) => {
+    res.json('delete to user endpoint')
+
+} );
+
+
+
 module.exports = router;
